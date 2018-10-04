49e291cfeb86e6bb6b66118514c2d5a8093178e2
chuanshanjun <chuanshanjun@gmail.com>
2018-10-04 21:58:32 +0800

    将动态数组变成支持范型

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -2,20 +2,20 @@
  * @author: Young
  * @create: 2018-10-01 12:10
  **/
-public class Array {
+public class Array<E> {
 
-    private int[] data;
+    private E[] data;
 
     private int size;
 
     // 构造函数 传入数组的容量capacity 构造数组
     public Array(int capacity) {
-        data = new int[capacity];
+        data = (E[]) new Object[capacity];
     }
 
     // 无参构造方法 默认容量capacity 为10
     public Array() {
-        data = new int[10];
+        data = (E[]) new Object[10];
     }
 
     // 获取数组中的元素个数
@@ -34,7 +34,7 @@ public class Array {
     }
 
     // 往所有元素的最后一位添加元素
-    public void addLast(int e) {
+    public void addLast(E e) {
         // 原始的addLast方法
 //        if (size == data.length) {
 //            throw new IllegalArgumentException("addLast failed, Array is full");
@@ -47,12 +47,12 @@ public class Array {
     }
 
     // 往所有元素的最前一位添加元素
-    public void addFirst(int e) {
+    public void addFirst(E e) {
         add(0, e);
     }
 
     // 在第inde个位置插入元素e
-    public void add(int index, int e) {
+    public void add(int index, E e) {
         if (size == data.length) {
             throw new IllegalArgumentException("addLast failed, Array is full");
         }
@@ -69,7 +69,7 @@ public class Array {
     }
 
     // 获取index 索引位置的元素
-    public int get(int index) {
+    public E get(int index) {
         if (index < 0 || index >= size) {
             throw new IllegalArgumentException("get failed, Index is Illegal");
         }
@@ -78,7 +78,7 @@ public class Array {
     }
 
     // 修改index 索引位置的元素
-    public void set(int index, int e) {
+    public void set(int index, E e) {
         if (index < 0 || index >= size) {
             throw new IllegalArgumentException("get failed, Index is Illegal");
         }
@@ -87,7 +87,7 @@ public class Array {
     }
 
     // 查找数组中元素是否有e
-    public boolean contains(int e) {
+    public boolean contains(E e) {
         for (int i = 0; i < size; i++) {
             if (data[i] == e) {
                 return true;
@@ -98,7 +98,7 @@ public class Array {
     }
 
     // 查找元素中e所在的索引, 如果不存在元素e 则返回-1
-    public int find(int e) {
+    public int find(E e) {
         for (int i = 0; i < size; i++) {
             if (data[i] == e) {
                 return i;
@@ -109,12 +109,12 @@ public class Array {
     }
 
     // 从数组中删除index位置的元素, 返回删除元素
-    public int remove(int index) {
+    public E remove(int index) {
         if (index < 0 || index >= size) {
             throw new IllegalArgumentException("Remove failed. Index is illegal");
         }
 
-        int res = data[index];
+        E res = data[index];
         for (int i = index + 1; i < size; i++) {
             data[i - 1] = data[i];
         }
@@ -123,17 +123,17 @@ public class Array {
     }
 
     // 从第一个删除
-    public int removeFirst() {
+    public E removeFirst() {
         return remove(0);
     }
 
     // 从最后一个删除
-    public int removeLast() {
+    public E removeLast() {
         return remove(size - 1);
     }
 
-    // 从元素中删除最后一个元素, 返回删除的元素
-    public void removeElement(int e) {
+    // 从元素中删除元素
+    public void removeElement(E e) {
         int index = find(e);
         if (index != -1) {
             remove(index);
