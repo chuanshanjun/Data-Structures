055e227bcba4de6c7d004f6a5e19627da159adc0
chuanshanjun <chuanshanjun@gmail.com>
2018-10-19 11:02:18 +0800

    05-Search-in-BST

---
+++ 06-Binary-Search-Tree/05-Search-in-BST/src/BST.java
@@ -0,0 +1,71 @@
+/**
+ * @author: Young
+ * @create: 2018-10-19 10:47
+ **/
+public class BST<E extends Comparable<E>> {
+
+    private class Node {
+        public Node left, right;
+        public E e;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+    // 向以node为根的二分搜索树中插入元素e，递归算法
+    // 返回插入新节点后二分搜索树的根
+    private Node add(Node node, E e) {
+        if (node == null) {
+            size++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
+        }
+
+        return node;
+    }
+
+    // 看二分搜索树中是否包含元素e
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    // 看以node为根的二分搜索树中是否包含元素e, 递归算法
+    private boolean contains(Node node, E e) {
+
+        // 终止条件1
+        if (node == null) {
+            return false;
+        }
+
+        if (e.equals(node.e)) { // 终止条件2
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else { // e.compareTo(node.e) > 0
+            return contains(node.right, e);
+        }
+    }
+}
